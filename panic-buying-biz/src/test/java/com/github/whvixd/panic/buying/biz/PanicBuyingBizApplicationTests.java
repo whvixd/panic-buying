e2d9e4445c15ba574b2a63887cf6b1ce4a409497
wangzhx <wangzhx@fxiaoke.com>
2020-03-01 16:59:07 +0800

    init

---
+++ panic-buying-biz/src/test/java/com/github/whvixd/panic/buying/biz/PanicBuyingBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.whvixd.panic.buying.biz;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanicBuyingBizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
