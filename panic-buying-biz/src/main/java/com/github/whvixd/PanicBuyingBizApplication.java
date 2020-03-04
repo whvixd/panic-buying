3954b69a0801769fa07343a578ae96bf728f80d9
wangzhx <wangzhx@fxiaoke.com>
2020-03-04 14:26:19 +0800

    修改启动类路径

---
+++ panic-buying-biz/src/main/java/com/github/whvixd/PanicBuyingBizApplication.java
@@ -0,0 +1,15 @@
+package com.github.whvixd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication//(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+public class PanicBuyingBizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanicBuyingBizApplication.class, args);
+	}
+
+}
