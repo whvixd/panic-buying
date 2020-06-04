503f696b5223b5a340dcb5bef68fd87791af9082
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-06-04 11:43:44 +0800

    添加test接口

--- panic-buying-biz/src/main/java/com/github/whvixd/panic/buying/controller/PingController.java
+++ panic-buying-biz/src/main/java/com/github/whvixd/panic/buying/controller/PingController.java
@@ -2,7 +2,9 @@ package com.github.whvixd.panic.buying.controller;
 
 import com.github.whvixd.panic.buying.model.Result;
 import com.github.whvixd.panic.buying.model.base.BaseResult;
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,5 +16,26 @@ public class PingController {
     @GetMapping("/ping")
     public BaseResult ping() {
         return Result.ok("hello world!");
+    }
+
+    // curl -X GET 'http://localhost:8080/test?name=test&nickName=test'
+    @RequestMapping("/test")
+    public BaseResult test(TestA testA,TestB testB){
+        if("test".equals(testA.getName())&&"test".equals(testB.getNickName())){
+            return Result.ok("hello world!");
+        }
+        return Result.fail("hello world!");
+
+    }
+
+    @Data
+    static class TestA{
+        String id;
+        String name;
+    }
+
+    @Data
+    static class TestB{
+        String nickName;
     }
 }
