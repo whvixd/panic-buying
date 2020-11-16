a0fc04ed939d3045caab3ab8251e94b55ccbcff1
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-11-16 21:06:16 +0800

    优化

--- panic-buying-biz/src/main/java/com/github/whvixd/panic/buying/controller/PingController.java
+++ panic-buying-biz/src/main/java/com/github/whvixd/panic/buying/controller/PingController.java
@@ -2,15 +2,19 @@ package com.github.whvixd.panic.buying.controller;
 
 import com.github.whvixd.panic.buying.model.Result;
 import com.github.whvixd.panic.buying.model.base.BaseResult;
+import com.github.whvixd.panic.buying.util.FastJsonUtil;
 import lombok.Data;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * Created by wangzhx on 2020/3/1.
  */
 @RestController
+@Slf4j
 public class PingController {
 
     @GetMapping("/ping")
@@ -22,22 +26,30 @@ public class PingController {
     // curl -X GET 'http://localhost:8080/test?name=test&nickName=test'
     // 从 入口 FrameworkServlet
     @RequestMapping("/test")
-    public BaseResult test(TestA testA,TestB testB){
-        if("test".equals(testA.getName())&&"test".equals(testB.getNickName())){
+    public BaseResult test(TestA testA, TestB testB) {
+        if ("test".equals(testA.getName()) && "test".equals(testB.getNickName())) {
             return Result.ok("hello world!");
         }
         return Result.fail("hello world!");
 
     }
 
+
+    @RequestMapping(value = "/test/map", method = RequestMethod.POST)
+    public BaseResult test(HttpServletRequest req, @RequestBody Map<String, Object> map) {
+        log.info(FastJsonUtil.toJson(map));
+        return Result.fail(FastJsonUtil.toJson(map));
+
+    }
+
     @Data
-    static class TestA{
+    static class TestA {
         String id;
         String name;
     }
 
     @Data
-    static class TestB{
+    static class TestB {
         String nickName;
     }
 }
