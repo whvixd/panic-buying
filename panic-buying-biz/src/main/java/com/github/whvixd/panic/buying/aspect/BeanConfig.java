ad37f44564df126a0f8ddbf645ae2d75286841f6
wangzhixiang <wangzhixiang_v@didiglobal.com>
2021-04-26 13:14:24 +0800

    优化

--- panic-buying-biz/src/main/java/com/github/whvixd/panic/buying/aspect/BeanConfig.java
+++ panic-buying-biz/src/main/java/com/github/whvixd/panic/buying/aspect/BeanConfig.java
@@ -2,6 +2,7 @@ package com.github.whvixd.panic.buying.aspect;
 
 import com.github.whvixd.panic.buying.manager.RedisClientManager;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +15,15 @@ public class BeanConfig {
     @ConditionalOnMissingBean
     public RedisClientManager instanceRedisClientManager(){
         return new RedisClientManager();
+    }
+
+    @Bean
+    public FilterRegistrationBean registerAccessFilter() {
+        FilterRegistrationBean<AccessFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new AccessFilter());
+        registration.setName("AccessFilter");
+        registration.addUrlPatterns("/*");
+        registration.setOrder(20);
+        return registration;
     }
 }
